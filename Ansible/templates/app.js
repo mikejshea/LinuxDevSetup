4788e76e9b9a352bf499153c089da2dfd3457515
mikejshea <mikejshea@gmail.com>
2017-03-27 14:27:10 -0400

    express

---
+++ Ansible/templates/app.js
@@ -0,0 +1,54 @@
+var express = require('express')
+var app = express()
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+
+app.get('/', function (req, res) {
+  //res.send('Hello World!')
+  //console.log('Example app listening on port 3000!')
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log('1:' + ifname + ':' + alias, iface.address);
+        res.status(200).send(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log('2:' + ifname, iface.address);
+        res.status(200).send(ifname + ': ' + iface.address);
+      }
+      ++alias;
+    });
+  });
+})
+
+app.listen(3000, function () {
+  //console.log('Example app listening on port 3000!')
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+      ++alias;
+    });
+  });
+})
--- Helpers/vmware_snapshot_base.sh
+++ Helpers/vmware_snapshot_base.sh
@@ -13,23 +13,32 @@ fi
 SNAPSHOT=$1
 
 echo Stopping VMs...
-vmrun stop "/mnt/md0/vms/gpd-mdb-1/gpd-mdb-1.vmx"
-vmrun stop "/mnt/md0/vms/gpd-mdb-2/gpd-mdb-2.vmx"
-vmrun stop "/mnt/md0/vms/gpd-mdb-3/gpd-mdb-3.vmx"
+# vmrun stop "/mnt/md0/vms/gpd-mdb-1/gpd-mdb-1.vmx"
+# vmrun stop "/mnt/md0/vms/gpd-mdb-2/gpd-mdb-2.vmx"
+# vmrun stop "/mnt/md0/vms/gpd-mdb-3/gpd-mdb-3.vmx"
+vmrun stop "/mnt/md0/vms/gpd-app-1/gpd-app-1.vmx"
+vmrun stop "/mnt/md0/vms/gpd-app-2/gpd-app-2.vmx"
+vmrun stop "/mnt/md0/vms/gpd-lb-1/gpd-lb-1.vmx"
 echo "VMs Stopped"
 sleep $WAIT_TIME
 
 echo Reverting to snapshot: $SNAPSHOT ...
-vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-mdb-1/gpd-mdb-1.vmx $SNAPSHOT
-vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-mdb-2/gpd-mdb-2.vmx $SNAPSHOT
-vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-mdb-1/gpd-mdb-1.vmx $SNAPSHOT
+# vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-mdb-1/gpd-mdb-1.vmx $SNAPSHOT
+# vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-mdb-2/gpd-mdb-2.vmx $SNAPSHOT
+# vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-mdb-3/gpd-mdb-3.vmx $SNAPSHOT
+vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-app-1/gpd-app-1.vmx $SNAPSHOT
+vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-app-2/gpd-app-2.vmx $SNAPSHOT
+vmrun -T ws revertToSnapshot /mnt/md0/vms/gpd-lb-1/gpd-lb-1.vmx $SNAPSHOT
 echo $SNAPSHOT Snapshot restored
 
 sleep $WAIT_TIME
 echo Start VMs...
-vmrun -T ws start /mnt/md0/vms/gpd-mdb-1/gpd-mdb-1.vmx
-vmrun -T ws start /mnt/md0/vms/gpd-mdb-2/gpd-mdb-2.vmx
-vmrun -T ws start /mnt/md0/vms/gpd-mdb-3/gpd-mdb-3.vmx
+# vmrun -T ws start /mnt/md0/vms/gpd-mdb-1/gpd-mdb-1.vmx
+# vmrun -T ws start /mnt/md0/vms/gpd-mdb-2/gpd-mdb-2.vmx
+# vmrun -T ws start /mnt/md0/vms/gpd-mdb-3/gpd-mdb-3.vmx
+vmrun -T ws start /mnt/md0/vms/gpd-app-1/gpd-app-1.vmx
+vmrun -T ws start /mnt/md0/vms/gpd-app-2/gpd-app-2.vmx
+vmrun -T ws start /mnt/md0/vms/gpd-lb-1/gpd-lb-1.vmx
 echo VMs Started
 
 echo Done
